6797470fcada4abe07fefad3359b2271c1385b0e
Dislexus <alee77234@gmail.com>
2019-01-19 02:21:58 -0600

    Attached fragment to base activity

--- app/src/main/java/com/main/hobbycollector/Features/BaseActivity.java
+++ app/src/main/java/com/main/hobbycollector/Features/BaseActivity.java
@@ -1,8 +1,11 @@
 package com.main.hobbycollector.Features;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.main.hobbycollector.Features.Dashboard.UserDashboardFragment;
 import com.main.hobbycollector.R;
 
 public class BaseActivity extends AppCompatActivity {
@@ -11,5 +14,14 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        UserDashboardFragment userDashboardFragment = new UserDashboardFragment();
+        fragmentManager.beginTransaction().add(R.id.fragment_container, userDashboardFragment).commit();
+    }
+
+    @Override
+    public void onAttachFragment(Fragment fragment) {
+        super.onAttachFragment(fragment);
     }
 }
